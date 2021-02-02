6720a0bd9443e4e64323dc27ba51247ca41931d9
Peter Crawford <pcraw1027@gmail.com>
2021-02-02 14:39:33 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -11,7 +11,7 @@ function updatePlaylist (playlist,key,value) {
 // key = "Slowdive"
 function removeFromPlaylist(playlist,key) {
   key = "Slowdive"
-  console.log(playlist.key[1])
+  console.log(key)
   delete playlist.key;
   return playlist
 }
